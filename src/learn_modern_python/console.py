94e61822a3992a1d0ce120f5c8fb66a7c3268708
Photon96 <>
2022-02-19 20:45:10 +0100

    Parametrize Wikipedia language version request

--- src/learn_modern_python/console.py
+++ src/learn_modern_python/console.py
@@ -1,18 +1,22 @@
 import textwrap
+import locale
 
 import click
 import requests
 
 from . import __version__
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 @click.command()
 @click.version_option(version = __version__)
-def main():
+@click.option('--language', help="Language version of Wikipedia. Default is your locale")
+def main(language):
     """The modern Python project"""
-
-    with requests.get(API_URL) as response:
+    if language is None:
+        local = locale.getlocale()
+        language = (local[0])[0:local[0].index("_")]
+    with requests.get(API_URL.format(language = language)) as response:
         status_code = response.status_code
         if (status_code != requests.codes.ok):
             click.secho(f"Bad request {status_code}", fg="red")
